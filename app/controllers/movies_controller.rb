131f1560681e03049793aa90c24ddeedcbb1dede
Rebecca Herrera <133678924+reherr@users.noreply.github.com>
2024-06-24 16:00:00 +0000

    added data-method delete

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -33,7 +33,7 @@ class MoviesController < ApplicationController
 
   def update
     the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_movie = Movie.where({ :id => the_id }).first
 
     the_movie.title = params.fetch("query_title")
     the_movie.description = params.fetch("query_description")
@@ -49,7 +49,7 @@ class MoviesController < ApplicationController
 
   def destroy
     the_id = params.fetch("path_id")
-    the_movie = Movie.where({ :id => the_id }).at(0)
+    the_movie = Movie.where({ :id => the_id }).first
 
     the_movie.destroy
 
